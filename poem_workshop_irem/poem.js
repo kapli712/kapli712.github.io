3e598b64507047b61d3180d5dd4a073d067059d9
kapli712 <kapli712@newschool.edu>
2021-03-26 18:28:11 +0300

    e

--- poem_workshop_irem/poem.js
+++ poem_workshop_irem/poem.js
@@ -1,2 +1,15 @@
-console.log('hiii!');
+let poemFragments = {
+    'noun': ["a mountain","Leaf","a grandma","a field","a cup","a horse","an ocean","a walnut","a desert","a cat","a branch","a diaper","socks","a cupcake","a pillow","a coffee","cat","girl","a cherry"],
+    'adjective': ["Tiring","rotund","hideous","breakable","sparkly","smooth","fluffy","fragile","delicious","lovely","hot","strange","smooth","ample","warm","colossal","rough","tiny","dark"],
+    'verb': ["Overthinking","thinking of","bouncing on","sculpting","waiting for","wanting","napping","thinking","hoping for","wanting","dancing","sleeping","frustra","brightening","skipping","looking for","swimming","thinking of","making"],
+    'emotion': ["sorrowful","Anxious","confused","yearning","angry","frustrated","melancholy","overwhelm","anxious","excited ","sleepy","triggered","excited","tired","joyful","disturbed","happy","delighted","upset"]
+  };
 
+function swapText(event) {
+	let itemClass = event.target.className;
+	console.log(itemClass);
+   
+  };
+
+let poem = document.querySelector('#poem');
+poem.addEventListener('mouseover', swapText);
