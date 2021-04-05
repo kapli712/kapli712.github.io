94db86aa6de761feaed19e3a38964d89cf544f37
kapli712 <kapli712@newschool.edu>
2021-04-06 01:28:45 +0300

    r

--- elastic_collections_irem/main.js
+++ elastic_collections_irem/main.js
@@ -1,87 +1,43 @@
-var Airtable = require("airtable");
-console.log(Airtable);
-
-var base = new Airtable({ apiKey: "keyjHMTNhKYdCvjzY" }).base(
-  "apphM5XrBVdWgCuXC"
-);
-
-//get the "books" table from the base, select ALL the records, and specify the functions that will receive the data
-base("anime").select({}).eachPage(gotPageOfAnime, gotAllAnime);
-
-// an empty array to hold our book data
-const anime = [];
-
-// callback function that receives our data
-function gotPageOfAnime(records, fetchNextPage) {
-  console.log("gotPageOfAnime()");
-  // add the records from this page to our books array
-  anime.push(...records);
-  // request more pages
-  fetchNextPage();
-}
-
-// call back function that is called when all pages are loaded
-function gotAllAnime(err) {
-  console.log("gotAllAnime()");
-
-  // report an error, you'd want to do something better than this in production
-  if (err) {
-    console.log("error loading anime");
-    console.error(err);
-    return;
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
-
-  // call function to show the books
-  showAnime();
 }
 
-// create the book-spines on the shelf
-function showAnime() {
-  console.log("showAnime()");
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
 
-  // find the shelf element
-  const shelf = document.getElementById("shelf");
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
 
-  // loop through the books loaded from the Airtable API
-  books.forEach((book) => {
-    // create the div, set its text and class
-    const div = document.createElement("div");
-    div.innerText = book.fields.title;
-    div.classList.add("book-spine");
-    // when the user clicks this book spine, call showBook and send the book data and this spine element
-    div.addEventListener("click", () => {
-      showBook(book, div);
-    });
-    // put the newly created book spine on the shelf
-    shelf.appendChild(div);
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
   });
-}
-
-// show the detail info for a book, and highlight the active book-spine
-function showBook(book, div) {
-  console.log("showBook()", book);
-
-  // find the book detail element
-  const bookDetail = document.getElementById("book-detail");
-
-  // populate the template with the data in the provided book
-  bookDetail.getElementsByClassName("title")[0].innerText = book.fields.title; //
-  bookDetail.getElementsByClassName("description")[0].innerText =
-    book.fields.description;
-  bookDetail.getElementsByClassName("more")[0].href = book.fields.more;
-  bookDetail.getElementsByClassName("cover-image")[0].src =
-    book.fields.cover_image[0].url;
-
-  // remove the .active class from any book spines that have it...
-  const shelf = document.getElementById("shelf");
-  const bookSpines = shelf.getElementsByClassName("active");
-  for (const bookSpine of bookSpines) {
-    bookSpine.classList.remove("active");
-  }
-  // ...and set it on the one just clicked
-  div.classList.add("active");
-
-  // reveal the detail element, we only really need this the first time
-  // but its not hurting to do it more than once
-  bookDetail.classList.remove("hidden");
 }
